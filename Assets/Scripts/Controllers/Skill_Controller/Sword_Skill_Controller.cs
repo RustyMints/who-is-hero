abe21aa782a22f1189cfc012951f3bb058f0acea
RustyMints <1974153157@qq.com>
2026-07-16 14:08:46 +0800

    恢复圆锯技能的平移效果：旋转时剑向扔出方向缓缓平移

--- Assets/Scripts/Controllers/Skill_Controller/Sword_Skill_Controller.cs
+++ Assets/Scripts/Controllers/Skill_Controller/Sword_Skill_Controller.cs
@@ -125,7 +125,7 @@ public class Sword_Skill_Controller : MonoBehaviour
             {
                 spinTimer -= Time.deltaTime;
 
-                //transform.position = Vector2.MoveTowards(transform.position, new Vector2(transform.position.x + spinDirection, transform.position.y), 1.5f * Time.deltaTime);
+                transform.position = Vector2.MoveTowards(transform.position, new Vector2(transform.position.x + spinDirection, transform.position.y), 1.5f * Time.deltaTime);
 
                 if (spinTimer < 0)
                 {
